5cee3994bf41919cbd686a882ccd7725ae0e76bf
Frolian Ernest <84269100+FROLIANI@users.noreply.github.com>
2026-01-19 22:09:06 +0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,34 @@
 @extends('layout.index')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title">Login</h5>
+        </div>
+
+        <div class="card-body">
+            <form action="{{ route('store.post') }}" method="POST">
+                @csrf
+
+                 <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+
+                   <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+
+                <button type="submit">Login Now</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+
